5da8aee202ace13c1b41e9fc7c65f724ff8c0916
Leonid Volnitsky <Leonid@Volnitsky.com>
2013-11-07 19:36:00 +0200

    boost search removed

--- bm/filter.cc
+++ bm/filter.cc
@@ -135,159 +135,142 @@ int main(int argc, char **argv) {
 	int written=0;
 	
 	while (true) {
-	    try {
-	        // Read stream_item from stdin
-	        stream_item.read(protocolInput.get());
-	
-	        string content;
-	        string actual_text_source = text_source;
-	        clog << "Reading stream item content from : " << text_source << endl;
-	        if (text_source == "clean_visible") {
-	            content = stream_item.body.clean_visible;
-	        } else if (text_source == "clean_html") {
-	            content = stream_item.body.clean_html;
-	        } else if (text_source == "raw") {
-	            content = stream_item.body.raw;
-	        } else {
-	            cerr << "Bad text_source :" << text_source <<endl;
-	            exit(-1);
-	        }
-	
-	        if (content.size() <= 0) {
-	            // Fall back to raw if desired text_source has no content.
-	            content = stream_item.body.raw;
-	            actual_text_source = "raw";
-	            if (content.size() <= 0) {
-	                // If all applicable text sources are empty, we have a problem and exit with an error
-	                cerr << cnt << " Error, doc id: " << stream_item.doc_id << " was empty." << endl;
-	                exit(-1);
-	            }
-	        }
+		try {
+	    		// Read stream_item from stdin
+	    		stream_item.read(protocolInput.get());
+	    		
+	    		string content;
+	    		string actual_text_source = text_source;
+	    		clog << "Reading stream item content from : " << text_source << endl;
+	    		if (text_source == "clean_visible") {
+	    		    content = stream_item.body.clean_visible;
+	    		} else if (text_source == "clean_html") {
+	    		    content = stream_item.body.clean_html;
+	    		} else if (text_source == "raw") {
+	    		    content = stream_item.body.raw;
+	    		} else {
+	    		    cerr << "Bad text_source :" << text_source <<endl;
+	    		    exit(-1);
+	    		}
+	    		
+	    		if (content.size() <= 0) {
+	    		    // Fall back to raw if desired text_source has no content.
+	    		    content = stream_item.body.raw;
+	    		    actual_text_source = "raw";
+	    		    if (content.size() <= 0) {
+	    		        // If all applicable text sources are empty, we have a problem and exit with an error
+	    		        cerr << cnt << " Error, doc id: " << stream_item.doc_id << " was empty." << endl;
+	    		        exit(-1);
+	    		    }
+	    		}
+            		
+	    		
+	    		
+	    		// NAIVE SEARCH ########################################################################
+            		
+	    		filter_names.name_to_target_ids["John Smith"] = vector<string>();
+	    		unordered_map<string, set<string> > target_text_map;
+            		
+	    		for(const auto& pr : filter_names.name_to_target_ids) {
+	    			const auto& name = pr.first;
+	    			//clog << name << "\n";
+            		        auto pos = std::search(content.begin(), content.end(),  name.begin(), name.end());
+	    			if (pos == content.end()) continue;
+            		
+	    			// found
+	    			clog << cnt << "  doc id: " << stream_item.doc_id;
+	    			clog << "\t" << name << "  (" << pos-content.begin() << ")\n";
+            		
+	    		
+	    			// mapping between canonical form of target and text actually found in document
+	    		
+	    			// For each of the current matches, add a label to the 
+	    			// list of labels.  A label records the character 
+	    			// positions of the match.
+	    			matches++;
+	    			
+	    			// Add the target identified to the label.  Note this 
+	    			// should be identical to what is in the rating 
+	    			// data we add later.
+	    			Target target;
+	    			target.target_id = "1";
+	    		
+	    			Label label;
+	    			label.target = target;
+	    		
+	    			// Add the actual offsets 
+	    			Offset offset;
+	    			offset.type = OffsetType::CHARS;
+	    			
+	    			offset.first = pos - content.begin();
+	    			offset.length = name.size();
+	    			offset.content_form = name;
+	    		
+	    			label.offsets[OffsetType::CHARS] = offset;
+	    			label.__isset.offsets = true;
+	    		
+	    			// Add new label to the list of labels.
+	    			stream_item.body.labels[annotatorID].push_back(label);
+	    		
+	    			// Map of actual text mapped 
+	    			target_text_map[target.target_id].insert(name);
+	    		
+	    		} // for all names
+	    		
+	    		// Add the rating object for each target that matched in a document
+	    		for ( auto match=target_text_map.begin(); match!=target_text_map.end(); ++match) {
+	    		    // Construct new rating
+	    		    Rating rating;
+	    		
+	    		    // Flag that it contained a mention
+	    		    rating.__set_contains_mention(true);
+	    		
+	    		    // Construct a target object for each match
+	    		    Target target;
+	    		    target.target_id = match->first;
+	    		    rating.target = target;
+	    		
+	    		    // Copy all the strings that matched into the mentions field
+	    		    copy(match->second.begin(), match->second.end(), back_inserter(rating.mentions));
+	    		
+	    		    // Subtle but vital, we need to do the following for proper serialization.
+	    		    rating.__isset.mentions = true;
+	    		
+	    		    // Add the annotator from above.
+	    		    rating.annotator = annotator;
+	    		
+	    		    // Push the new rating onto the rating list for this annotator.
+	    		    stream_item.ratings[annotatorID].push_back(rating);
+	    		}
+	    		
+	    		if (not negate) { 
+	    		        // Write stream_item to stdout if it had any ratings
+	    		        if (target_text_map.size() > 0) {
+	    		            stream_item.write(protocolOutput.get());
+	    		            written++;
+	    		        }
+	    		} else if (target_text_map.size() == 0) {
+	    			// Write stream_item to stdout if user requested
+	    		       	// to show ones that didn't have any matches
+	    			stream_item.write(protocolOutput.get());
+	    			written++;
+	    		}
+	    		
+	    		// Increment count of stream items processed
+	    		cnt++;
+	    	}
 
-		
-	     
-// NAIVE SEARCH ############################################################################################################
-
-		filter_names.name_to_target_ids["John Smith"] = vector<string>();
-
-		for(const auto& pr : filter_names.name_to_target_ids) {
-			const auto& name = pr.first;
-			//clog << name << "\n";
-                        auto pos = std::search(content.begin(), content.end(),  name.begin(), name.end());
-			if (pos == content.end()) continue;
-
-			// found
-			clog << cnt << "  doc id: " << stream_item.doc_id;
-			clog << "\t" << name << "  (" << pos-content.begin() << ")\n";
+		catch (TTransportException e) {
+			// Vital to flush the buffered output or you will lose the last one
+			transportOutput->flush();
+			clog << "Total stream items processed: " << cnt << endl;
+			clog << "Total matches : " << matches << endl;
+			clog << "Total stream items written         : " << written << endl;
+			if (negate) {
+				clog << " (Note, stream items written were non-matching ones)" << endl;
+			}
+			break;
 		}
-// BOOST SEARCH ############################################################################################################
-	        // search content 
-	        boost::regex rgx("John.{0,5}Smith", boost::regex_constants::icase);
-	        boost::smatch m;
-	        boost::regex_iterator<std::string::iterator> rit ( content.begin(), content.end(), rgx);
-	        boost::regex_iterator<std::string::iterator> rend;
-	
-	        // mapping between canonical form of target and text actually found in document
-	        unordered_map<string, set<string> > target_text_map;
-	
-	        // For each of the current matches, add a label to the 
-	        // list of labels.  A label records the character 
-	        // positions of the match.
-	        while (rit!=rend) {
-	    	clog << "Found: " << rit->str() << " at offset: " << rit->position() << std::endl;
-	    	matches++;
-	            
-	            // Add the target identified to the label.  Note this 
-	            // should be identical to what is in the rating 
-	            // data we add later.
-	            Target target;
-	            target.target_id = "1";
-	
-	            Label label;
-	            label.target = target;
-	
-	            // Add the actual offsets 
-	            Offset offset;
-	            offset.type = OffsetType::CHARS;
-	    	
-	            offset.first = (int) rit->position();
-	            offset.length = (int) rit->length();
-	            offset.content_form = rit->str(); 
-	
-	            label.offsets[OffsetType::CHARS] = offset;
-	            label.__isset.offsets = true;
-	
-	            // Add new label to the list of labels.
-	            stream_item.body.labels[annotatorID].push_back(label);
-	
-	    	// Map of actual text mapped 
-	            target_text_map[target.target_id].insert(rit->str());
-	
-	    	// Incerment the loop iterator
-	            ++rit;
-	    	
-	            }
-	        
-	        // Add the rating object for each target that matched in a document
-	        // Reminder:
-	        // match->first is key
-	        // match->second is value
-	        for( unordered_map<string, set<string> >::iterator match=target_text_map.begin(); 
-	             match!=target_text_map.end(); 
-	             ++match)
-	        {
-	            // Construct new rating
-	            Rating rating;
-	
-	            // Flag that it contained a mention
-	            rating.__set_contains_mention(true);
-	
-	            // Construct a target object for each match
-	            Target target;
-	            target.target_id = match->first;
-	            rating.target = target;
-	
-	            // Copy all the strings that matched into the mentions field
-	            copy(match->second.begin(), match->second.end(), back_inserter(rating.mentions));
-	
-	            // Subtle but vital, we need to do the following for proper serialization.
-	            rating.__isset.mentions = true;
-	
-	            // Add the annotator from above.
-	            rating.annotator = annotator;
-	
-	            // Push the new rating onto the rating list for this annotator.
-	            stream_item.ratings[annotatorID].push_back(rating);
-	        }
-	
-	        if (not negate) { 
-	                // Write stream_item to stdout if it had any ratings
-	                if (target_text_map.size() > 0) {
-	                    stream_item.write(protocolOutput.get());
-	                    written++;
-	                }
-	        } else if (target_text_map.size() == 0) {
-	            // Write stream_item to stdout if user requested
-	            // to show ones that didn't have any matches
-	            stream_item.write(protocolOutput.get());
-	            written++;
-	        }
-	
-	        // Increment count of stream items processed
-	        cnt++;
-	    }
-	    catch (TTransportException e) {
-	        // Vital to flush the buffered output or you will lose the last one
-	        transportOutput->flush();
-	        clog << "Total stream items processed: " << cnt << endl;
-	        clog << "Total matches : " << matches << endl;
-	        clog << "Total stream items written         : " << written << endl;
-	        if (negate) {
-	                clog << " (Note, stream items written were non-matching ones)" << endl;
-	        }
-	        break;
-	    }
 	}
-	return 0;
 }
 
